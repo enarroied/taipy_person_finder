05e034452d4aa523266a724c234afa409e99093f
enarroied <eric.narro.ied@gmail.com>
2025-09-13 12:13:57 +0200

    added datasets

--- create_data/create_fake_data.py
+++ create_data/create_fake_data.py
@@ -6,6 +6,12 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _():
     import pandas as pd
     from faker import Faker
 
@@ -14,7 +20,24 @@ def _():
 
     num_rows = 50000
 
-    return fake, num_rows, pd
+    return Faker, fake, num_rows, pd
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+    # Create datasets for the application
+
+    This notebook creates dataset files for the Taipy 🔎 Person Finder application. This application lets users look for a person in a dataset, using string similarity algorithms (Jaro-Winkler similarity, mainly; we might introduce others as well). 
+    The application simulates a company with a large dataset of people coming from its information system. We code a Taipy application to let users look for people in the company dataset with two methods: by direct querying of the dataset (looking for one person in the dataset), or by uploading a file (CSV or parquet) and looking for a match.
+
+    This notebook uses `Faker` to create the company dataset; it also generates two demonstration files (a CSV and a parquet file) to compare to the company data.
+
+    ## Create the Application's dataset
+    """
+    )
+    return
 
 
 @app.cell
@@ -40,7 +63,40 @@ def _(fake, num_rows, pd):
 
 
 @app.cell
-def _():
+def _(mo):
+    mo.md(
+        r"""
+    ## Create the query data
+
+    The following datasets can be uploaded to the application to look for matches.
+
+    We mainly need the dataset to have first and last names in it. We also add phone numbers to simulate extra columns, because: why not?
+
+    We use a **French locale code to make sure our names and family names are different from the ones used in the main company dataset**.
+    """
+    )
+    return
+
+
+@app.cell
+def _(Faker):
+    Faker.seed(2)
+    fake_seed_2 = Faker('fr_FR')
+
+    return (fake_seed_2,)
+
+
+@app.cell
+def _(fake_seed_2, num_rows, pd):
+    trial_data  = {
+        "first_name": [fake_seed_2.first_name() for _ in range(num_rows)],
+        "family_name": [fake_seed_2.last_name() for _ in range(num_rows)],
+        "phone": [fake_seed_2.phone_number() for _ in range(num_rows)],
+    }
+
+    trial_data = pd.DataFrame(trial_data)
+    trial_data.to_parquet("../src/data/trial_dataset.parquet", index=False)
+    trial_data.to_csv("../src/data/trial_dataset.csv", index=False)
     return
 
 
