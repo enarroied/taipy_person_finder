0129cc8d7820181425a068497a905c3461524346
enarroied <eric.narro.ied@gmail.com>
2026-01-23 21:48:31 +0100

    separate callback concerns

--- src/callbacks/look_for_person_callback.py
+++ src/callbacks/look_for_person_callback.py
@@ -1,12 +1,16 @@
 from algorithms import RetrieveSimilarNames, normalize_name
 
 
-def look_for_person_callback(state):
+def look_for_person(name, threshold_person):
     runner = RetrieveSimilarNames()
+    df_similar_person = runner.run(name, threshold_person)
+    df_similar_person["jaro_winkler_similarity_score"] = df_similar_person[
+        "jaro_winkler_similarity_score"
+    ].round(2)
+    return df_similar_person
+
+
+def look_for_person_callback(state):
     with state as s:
         name = normalize_name(s.person_name)
-        df_similar_person = runner.run(name, s.threshold_person)
-        df_similar_person["jaro_winkler_similarity_score"] = df_similar_person[
-            "jaro_winkler_similarity_score"
-        ].round(2)
-        s.df_similar_person = df_similar_person
+        s.df_similar_person = look_for_person(name, s.threshold_person)
